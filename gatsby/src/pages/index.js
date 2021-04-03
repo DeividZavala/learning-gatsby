248da80e86bee5523cc96174a7cce24b758aac52
David <crusedmoss@gmail.com>
2021-04-02 23:24:33 -0600

    pages and navigation

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Nav />
+      <p>Home page de mi sitio</p>
+    </div>
+  );
+}
